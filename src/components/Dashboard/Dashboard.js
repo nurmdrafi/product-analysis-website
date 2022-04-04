343ec17bb56d3c894b30fea82ed28ff60e4158de
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-04 20:59:30 +0600

    redesign dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,6 +9,8 @@ import {
   Tooltip,
   Legend,
   Bar,
+  PieChart,
+  Pie,
 } from "recharts";
 import "./Dashboard.css";
 
@@ -21,10 +23,32 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="dashboard container ">
+    <div className="dashboard container">
       <h1 className="text-center">DashBoard</h1>
-      <div className="row my-5">
-        <div className="col-md-6 col-sm-12">
+      <div className="row row-cols-1 row-cols-md-1 row-cols-lg-2 my-5">
+        <div className="col">
+          <h5 className="text-center text-uppercase text-primary text-opacity-75 my-3">
+            month wise sell
+          </h5>
+          <LineChart width={400} height={400} data={charts}>
+            <Line
+              type="monotone"
+              dataKey="sell"
+              stroke="#8884d8"
+              strokeWidth={2}
+              activeDot={{ r: 8 }}
+            />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Legend />
+            <Tooltip />
+          </LineChart>
+        </div>
+
+        <div className="col">
+          <h5 className="text-center text-uppercase text-primary text-opacity-75 my-3">
+            Investment vs Revenue
+          </h5>
           <LineChart
             width={400}
             height={400}
@@ -36,20 +60,25 @@ const Dashboard = () => {
               dataKey="investment"
               stroke="#8884d8"
               activeDot={{ r: 8 }}
+              strokeWidth={2}
             />
-            <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+            <Line
+              type="monotone"
+              dataKey="revenue"
+              stroke="#82ca9d"
+              strokeWidth={2}
+            />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <XAxis dataKey="month" />
             <YAxis />
             <Legend />
             <Tooltip />
           </LineChart>
-          <h5 className="text-center my-2">
-            Month wise Investment and Revenue
-          </h5>
         </div>
-
-        <div className="col-md-6 col-sm-12">
+        <div className="col">
+          <h5 className="text-center text-uppercase text-primary text-opacity-75 my-3">
+            Investment vs Revenue
+          </h5>
           <BarChart
             width={400}
             height={400}
@@ -62,13 +91,39 @@ const Dashboard = () => {
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" />
+            <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="sell" fill="#82ca9d" />
+            <Bar dataKey="investment" fill="#8884d8" />
+            <Bar dataKey="revenue" fill="#82ca9d" />
           </BarChart>
-          <h5 className="text-center my-2">Month wise sell</h5>
+        </div>
+        <div className="col">
+          <h5 className="text-center text-uppercase text-primary text-opacity-75 my-3">
+            Investment vs Revenue
+          </h5>
+          <PieChart width={400} height={400}>
+            <Pie
+              data={charts}
+              dataKey="investment"
+              cx="50%"
+              cy="50%"
+              outerRadius={60}
+              fill="#8884d8"
+            />
+            <Pie
+              data={charts}
+              dataKey="revenue"
+              cx="50%"
+              cy="50%"
+              innerRadius={70}
+              outerRadius={90}
+              fill="#82ca9d"
+              label
+            />
+            <Tooltip />
+          </PieChart>
         </div>
       </div>
     </div>
